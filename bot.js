09ed886e6ddb0c567b6675d4990d77f4ff76456f
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-09 01:03:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -682,6 +682,57 @@ client.on('message' , async (message) => {
 
 
 
+client.on('message', msg => { 
+  if (msg.content.startsWith(`*اقتراح`)) {
+     let args = msg.content.split(" ").slice(1);
+    if (!args[1]) return msg.reply(`يجب كتابه الاقتراح`) 
+    if (msg.guild.channels.find('name', 'الاقتراحات')) {
+      msg.guild.channels.find('name', 'الاقتراحات').send(`
+    الاقتراح من : ${msg.member}
+    الاقتراح : **${args.join(" ").split(msg.mentions.members.first()).slice(' ')}**
+    `)
+    }
+  }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message' , async (message) => {
+  var prefix = "*";
+ if (message.content.startsWith(prefix + 'bot')) {
+ const os = require('os');
+    const arch = os.arch()
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+
+    let totalSeconds = process.uptime();
+    let realTotalSecs = Math.floor(totalSeconds % 60);
+    let days = Math.floor((totalSeconds % 31536000) / 86400);
+    let hours = Math.floor((totalSeconds / 3600) % 24);
+    let mins = Math.floor((totalSeconds / 60) % 60);
+
+    var ping = client.ping
+    message.channel.send(`\n= Memory usage: ${Math.round(used * 100) / 100}MB\n= Ping: ${ping}\n= Uptime: Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${realTotalSecs}\n= Node: ${process.version}\n= Library: discord.js\n= ARCH: ${arch}\n= Plataform: ${os.platform}\n= Servers: ${client.guilds.size}\n= Users: ${client.users.size}`, {
+        code: 'AsciiDoc'
+    })
+
+}
+});
+
+
+
+
+
+
+
 
  
   

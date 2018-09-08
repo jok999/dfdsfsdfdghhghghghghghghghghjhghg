62112491361f6b49cbdc5059c6a1888fe5a2fe1c
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-08 17:49:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+
 
 
 const discord = require('discord.js');

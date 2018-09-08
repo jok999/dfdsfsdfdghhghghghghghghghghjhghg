703925059c6972f7f1623daa37e7f60d9fe504ac
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-08 14:19:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,172 +8,22 @@ client.on('ready', () => {
 
 
 
-client.on('message' , async (message) => {
-  var prefix = "%";
- if (message.content.startsWith(prefix + 'prm')) {
- const os = require('os');
-    const arch = os.arch()
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-
-    let totalSeconds = process.uptime();
-    let realTotalSecs = Math.floor(totalSeconds % 60);
-    let days = Math.floor((totalSeconds % 31536000) / 86400);
-    let hours = Math.floor((totalSeconds / 3600) % 24);
-    let mins = Math.floor((totalSeconds / 60) % 60);
-
-    var ping = client.ping
-    message.channel.send(`\n= Memory usage: ${Math.round(used * 100) / 100}MB\n= Ping: ${ping}\n= Uptime: Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${realTotalSecs}\n= Node: ${process.version}\n= Library: discord.js\n= ARCH: ${arch}\n= Plataform: ${os.platform}\n= Servers: ${client.guilds.size}\n= Users: ${client.users.size}`, {
-        code: 'AsciiDoc'
-    })
-
-}
-});
-
-
-
-
-
-
-
-
-
-client.on("message", message => {
-    var prefix = "%";
-  if (message.author.bot) return;
- if(!message.channel.guild)return;
-  if (!profile[message.author.id]) profile[message.author.id] = {
-    tite: 'HypeLC User',
-    rep: 0,
-   reps: 'NOT YET',
-   lastDaily:'Not Collected',
-    level: 0,
-    points: 0,
-    credits: 1,
-  };
-fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-});
-client.on("message", (message) => {
-  let men = message.mentions.users.first()
-  if (message.author.bot) return;
-    if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;
-if (message.content.startsWith(prefix + 'credit')) {
-  if(men) {
-  if (!profile[men.id]) profile[men.id] = {
-   lastDaily:'Not Collected',
-   credits: 1,
- };
-  }
-  if(men) {
-message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
-} else {
- message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
-}
-}
-if(message.content.startsWith(prefix + "daily")) {
-
-
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
-   profile[message.author.id].lastDaily = moment().format('day')
-   profile[message.author.id].credits += 310
-    message.channel.send(`**${message.author.username} you collect your \`310\` :dollar: daily pounds**`)
-} else {
-    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
-}
-}
-let cont = message.content.slice(prefix.length).split(" ");
-let args = cont.slice(2);
-let sender = message.author
-if(message.content.startsWith(prefix + 'trans')) {
-          if (!args[0]) {
-            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-         return;
-           }
-        // We should also make sure that args[0] is a number
-        if (isNaN(args[0])) {
-            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-             }
-             if(profile[message.author.id].credits < args[0]) return message.channel.send("**Your Credits is not enough  that**")
-if(args[0].startsWith("-")) return  message.channel.send('**!! I Cant Do it**');
-				 let defineduser = '';
-            let firstMentioned = message.mentions.users.first();
-            defineduser = (firstMentioned)
-            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
-            if(defineduser.id === message.author.id) return message.channel.send("***Transfering to your self hah ?!***")
-            var mentionned = message.mentions.users.first();
-if (!profile[sender.id]) profile[sender.id] = {}
-if (!profile[sender.id].credits) profile[sender.id].credits = 310;
-fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
-if (err) console.error(err);
-})
-var x = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' , '3489' , '1382' , '7389' , '8762' , '0889' , '0388' , '3316' , '0976' , '8603' , '1842' , '4565' , '9524' , '9524' , '0964' , '5930' , '5678' , '9567' , '6099' , '7058' , '0001' , '1324' , '9834' , '7668' , '0378' , '7055' , '9733' , '9876' , '9846' , '9685' , '8574' , '8975' , '9845' , '9862' , '0069' , '0807' , '0673' , '0813' , '1235' , '6879'];
-var x2 = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' , '3489' , '1382' , '7389' , '8762' , '0889' , '0388' , '3316' , '0976' , '8603' , '1842' , '4565' , '9524' , '9524' , '0964' , '5930' , '5678' , '9567' , '6099' , '7058' , '0001' , '1324' , '9834' , '7668' , '0378' , '7055' , '9733' , '9876' , '9846' , '9685' , '8574' , '8975' , '9845' , '9862' , '0069' , '0807' , '0673' , '0813' , '1235' , '6879'];
-        var x3 = Math.floor(Math.random()*x.length)
-        message.channel.send(` \`${args}\`** : الملبغ**  \n \`${x[x3]}\` ** : اكتب الرقم التالي حتي تتم عملية التحويل **`).then(msg1=> { 
-        var r = message.channel.awaitMessages(msg => msg.content == x2[x3], { maxMatches : 1, time : 60000, errors : ['time'] })
-        r.catch(() => {
-            message.delete()
-            r.delete()
-            msg.delete()
-            message.channel.sendEmbed(embed)
-        })
-        r.then(s=> {
-      var mando = message.mentions.users.id;
-      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
-      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
-      profile[defineduser.id].credits += (+args[0]);
-      profile[sender.id].credits += (-args[0]);
-      let mariam = message.author.username
-message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
-mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${args[0]} from user ${message.author.username} ; (ID (${message.author.id})\`\`\``);
-               message.channel.sendEmbed(embed)
-        })
-        })
-        
-		
-
-
-
-
-}
-
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-  client.on('message', msg => {
-    var prefix = "%"
-  if(msg.content.startsWith (prefix  + 'server')) {
-  let embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setThumbnail(msg.guild.iconURL)
-  .setTitle(`Showing Details Of  **${msg.guild.name}**`)
-  .addField(':globe_with_meridians: نوع السيرفر', `[** ${msg.guild.region}**]`,true)
-  .addField(':medal: الرتب',`[** __${msg.guild.roles.size}__ **]`,true)
-  .addField(':red_circle: عدد الاعضاء',`[** __${msg.guild.memberCount}__ **]`,true)
-  .addField(':large_blue_circle: عدد الاعضاء الاونلاين',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
-  .addField(':pencil: الرومات الكتابية',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
-  .addField(':microphone: رومات الصوت',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
-  .addField(':crown: الأونـر',`**${msg.guild.owner}**`,true)
-  .addField(':id: ايدي السيرفر',`**${msg.guild.id}**`,true)
-  .addField(':date: تم عمل السيرفر في',msg.guild.createdAt.toLocaleString())
-  msg.channel.send(embed);
-  }
-});
-
+        client.on('message', message => {
+          var prefix = '^^';
+          if (message.content.startsWith(prefix + 'موسيقى')) {
+      let embed = new Discord.RichEmbed()
+      .addField('%play' ,'   :headphones:تشغيل الاغنيه باسم او برابط:headphones: ')
+      .addField('%stop' ,'    :mute: ايقاف الاغنيه:mute:   ')
+      .addField('%skip' ,'     :play_pause: تخطي الاغنيه :play_pause:  ')
+      .addField('%pause' ,'    :clock1: ايقاف الاغنيه مؤقت:clock1:  ')
+      .addField('%resume' ,'    :musical_note: تكملةالاغنيه:musical_note:  ')
+      .addField('%queue' ,'    :pencil: اظهار قائمة التشغيل :pencil:   ')
+      .addField('%np' ,'    :page_facing_up: اظهار الاغنية اللي انت مشغلها حاليا:page_facing_up:  ')
+      .setColor('RANDOM')
+      .setTitle('═════ஜ۩۞۩ஜ══════════ஜ۩۞۩ஜ═════')
+      message.channel.send(embed)
+      }
+  });
 
 
 

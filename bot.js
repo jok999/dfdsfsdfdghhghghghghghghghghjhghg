431fcd08081ddc44debc13b812e011458943dddd
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-19 16:15:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const UserBlocked = new Set(); 
-const pretty = require('pretty-ms')
-,ti={};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -18,82 +15,7 @@ client.on('ready', () => {
 
 
 
- const math = require('math-expression-evaluator');  
-client.on('message', message => {
-	var prefix = "%";
-if (message.content.startsWith(prefix + 'calc')) {
-    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-           var Canvas = module.require('canvas');
-           var jimp = module.require('jimp');
-   
-    const w = ['./math.png'];
-   
-            let Image = Canvas.Image,
-                canvas = new Canvas(802, 404),
-                ctx = canvas.getContext('2d');
-            ctx.patternQuality = 'bilinear';
-            ctx.filter = 'bilinear';
-            ctx.antialias = 'subpixel';
-            ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-            ctx.shadowOffsetY = 2;
-            ctx.shadowBlur = 2;
-            fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                if (err) return console.log(err);
-                let BG = Canvas.Image;
-                let ground = new Image;
-                ground.src = Background;
-                ctx.drawImage(ground, 0, 0, 802, 404);
-   
-    })
-        let args = message.content.split(" ").slice(1);
-       const question = args.join(' ');
-   if (args.length < 1) {
-       message.reply('Specify a equation, please.\n\ Ex: #calc 5+5 ' );
-} else {    let answer;
-   try {
-       answer = math.eval(question);
-   } catch (err) {
-       message.reply(`Error: ${err}`);
-   }
-var ment = message.mentions.users.first();
-           var getvalueof;
-           if(ment) {
-             getvalueof = ment;
-           } else {
-             getvalueof = message.author;
-           }
-                                          let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
-                                            jimp.read(url, (err, ava) => {
-                                                if (err) return console.log(err);
-                                                ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                                                    if (err) return console.log(err);
-                                                                             
-                           
-                                                    ctx.font = '42px Arial Bold';//Name ,_,
-                                                    ctx.fontSize = '50px';
-                                                    ctx.fillStyle = "#ffffff";
-                                                    ctx.fillText(`${answer}`,108, 130);
-                                                   
-                                                   
-                                                     ctx.font = '25px Arial Bold';//Name ,_,
-                                                    ctx.fontSize = '30px';
-                                                    ctx.fillStyle = "#ffffff";
-                                                    ctx.fillText(`${question}`,105, 90);
- 
-                           
-                                ctx.beginPath();
-                                ctx.stroke();
-                              message.channel.sendFile(canvas.toBuffer());
-                           
-                           
-                         
-                           
-                            })
-                           
-                            })
-}
-}
-});
+
 
 
 
@@ -113,7 +35,7 @@ client.on('message', message => {
             var Canvas = module.require('canvas');
             var jimp = module.require('jimp');
     
-     const w = ['./img/ID1.png','./Dreams/img/ID2.png','./img/ID3.png','./img/ID4.png','./img/ID5.png'];
+     const w = ['./img/ID1.png','./img/ID2.png','./img/ID3.png','./img/ID4.png','./img/ID5.png'];
     
              let Image = Canvas.Image,
                  canvas = new Canvas(802, 404),

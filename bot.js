c038c4aba0640c910951683b3bfa633a18215a1b
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-29 18:10:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
